6889c5a4c5679b55448e4aa6659014d93087e51c
99002632 <71488754+99002632@users.noreply.github.com>
2020-09-19 10:36:00 +0530

    Update main.c
    changes done

--- imple/main.c
+++ imple/main.c
@@ -6,70 +6,70 @@ FILE * fp;
 Employee emp;
 int option;
 char another;
-if((fp=fopen("employeeInfo.txt","rb+"))==NULL)
+if((fp=fopen("employeeInfo.txt","rb+"))== NULL)
 {
- if((fp=fopen("employeeInfo.txt","wb+"))==NULL)
+ if((fp=fopen("employeeInfo.txt","wb+"))== NULL)
  {
- printf("can't open file");
+ printf("Opening file error");
  return 0;
  }
 }
-char username[10],password[10];
+char user[10],
+char passcode[10];
 printHead();
 welcome();
 printHead();
-printf("\n\t\t\t\t Login Screen");
-printf("\n\t\t\t Enter Your Credential");
-printf("\n\n\n\t\tUsername: ");
-scanf("%s",username);
-printf("\n\t\tPassword: ");
-int i;
-i=0;
+printf("\n\t\t\t\t login screen");
+printf("\n\t\t\t enter ur details");
+printf("\n\n\n\t\t enter username: ");
+scanf("%s",user);
+printf("\n\t\tPasscode: ");
+int imp;
+imp=0;
 do
 {
-password[i] = getch();
-if(password[i] == 13 )
+passcode[imp] = getch();
+if(passcode[imp] == 13 )
 {
 break;
 }
-else if(password[i]==8 && i>0)
+else if(passcode[imp]==8 && imp>0)
 {
-printf("%c%c%c",8,32,8);
-i--;
+printf("%c %c %c",8,32,8);
+imp--;
 }
 else
 {
 printf("*");
-i++;
+imp++;
 }
-}while(password[i]!=13);
-password[i] = '\0';
-if(((strcasecmp(username,"admin"))==0)&&((strcasecmp(password,"pass")==0)))
+}while(passcode[imp]!=13);
+password[imp] = '\0';
+if(((strcasecmp(user,"admin"))==0) && ((strcasecmp(passcode,"pass")==0)))
 {
  while(1)
 {
  printHead();
- printf("\n\t\t\t\tMain Menu");
- printf("\n\n\n");
- printf("\n\n\t\t\t1. Add Employee");
- printf("\n\n\t\t\t2. Delete Employee");
- printf("\n\n\t\t\t3. Modify Employee");
- printf("\n\n\t\t\t4. Display Employee List");
- printf("\n\n\t\t\t5. Search Record");
- printf("\n\n\t\t\t6. Display Basic Info");
- printf("\n\n\t\t\t7. Display Basic Contact Info");
- printf("\n\n\t\t\t8. List of Male Employee");
- printf("\n\n\t\t\t9. List of Female Employee");
- printf("\n\n\t\t\t10. List of Employee From Dhaka");
- printf("\n\n\t\t\t11. List of Employee From Others
-District");
- printf("\n\n\t\t\t12. List of Employee of Main Branch");
- printf("\n\n\t\t\t13. List of Employee of Others
-Branch");
- printf("\n\n\t\t\t0. EXIT");
- printf("\n\n\t\tEnter Your Option :--> ");
- scanf("%d",&option);
- switch(option)
+ printf("\n\t\t\t\t main page");
+ printf("\n\n\n ");
+ printf("\n\n\t\t\t 1. add emp");
+ printf("\n\n\t\t\t 2. delete emp");
+ printf("\n\n\t\t\t 3. modify Emp");
+ printf("\n\n\t\t\t 4. disp emp list");
+ printf("\n\n\t\t\t 5. Search Record");
+ printf("\n\n\t\t\t 6. disp basic details");
+ printf("\n\n\t\t\t 7. disp gender");
+ printf("\n\n\t\t\t 8. no of Male emp");
+ printf("\n\n\t\t\t 9. no of Female emp");
+ printf("\n\n\t\t\t 10. no of Emp from abc");
+ printf("\n\n\t\t\t 11. no of  Emp from diff district");
+ printf("\n\n\t\t\t 12. no of Emp of main branch");
+ printf("\n\n\t\t\t 13. no of Emp of others branch");
+ printf("\n\n\t\t\t  0. done");
+ printf("\n\n\t\t Enter Your Option :--> ");
+ scanf("%d",&choice);
+        
+ switch(choice)
  {
  case 0: return 1;
  break;
@@ -91,7 +91,7 @@ Branch");
  break;
  case 9: femaleemp(fp);
  break;
- case 10: frmdhaka(fp);
+ case 10: frmdiff(fp);
  break;
  case 11: frmors(fp);
  break;
@@ -99,15 +99,15 @@ Branch");
  break;
  case 13: otherbr(fp);
  break;
- default: printf("\n\t\tYou Pressed wrong key");
- printf("\n\t\tProgram terminated");
+ default: printf("\n\t\t wrong key pressed");
+ printf("\n\t\t prog terminated");
  getch();
  exit(0);
  }
 }
 }
 else {
- printf("\n\t\tLogin Failed");
+ printf("\n\t\t login error");
 }
 return 1;
 }
