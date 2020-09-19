cfd3718e6095df8f01533905cca1ea2743e88adf
99002632 <71488754+99002632@users.noreply.github.com>
2020-09-19 10:15:21 +0530

    Update emp.h
    emp file changed

--- imple/include/emp.h
+++ imple/include/emp.h
@@ -1,4 +1,8 @@
-• #include<stdio.h>
-• #include<string.h>
-• #include<stdlib.h>
-• #include<math.h>
+#ifndef __EMP_H__
+#define __EMP_H__
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#endif
